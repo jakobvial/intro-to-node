13c0168601290885d1c2fc685c0d47c6237c0bdd
Jakob Viktor Albrechtsen <jakobvial@gmail.com>
2022-09-25 19:38:58 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import superheroes from "superheroes";
+import superVillains from "supervillains";
+
+let mySuperVillainName = superVillains.random();
+let mySuperheroName = superheroes.random();
+
+console.log("Superhero: " +  mySuperheroName);
+console.log("Super villain: " + mySuperVillainName);
